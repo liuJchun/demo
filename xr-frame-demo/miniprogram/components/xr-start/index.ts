105e64381f8071d0a79b038a7de05a433be75f6c
jianchun.liu <943309727@qq.com>
2022-12-14 15:04:08 +0800

    增加xr-frame的demo

---
+++ xr-frame-demo/miniprogram/components/xr-start/index.ts
@@ -0,0 +1,23 @@
+// components/xr-start.ts
+Component({
+    /**
+     * 组件的属性列表
+     */
+    properties: {
+
+    },
+
+    /**
+     * 组件的初始数据
+     */
+    data: {
+
+    },
+
+    /**
+     * 组件的方法列表
+     */
+    methods: {
+
+    }
+})
